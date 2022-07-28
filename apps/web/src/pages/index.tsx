caef77cc8c57401acfcee3bd7b9ca8160d5c03c9
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2022-07-28 10:29:07 -0400

    fix

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from "next";
+import { NextPage } from "next";
+import React from "react";
 
 const Home: NextPage = () => {
-  return <></>;
+  return <div></div>;
 };
 
 export default Home;
